635b86acc1ae8e621775ca94d87d741940efb238
Tyrell James <nintyboy@gmail.com>
2023-11-25 13:56:25 +0000

    feat(styled): Added Styled Components - Now need to fix Android Build

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,23 @@ const { getDefaultConfig } = require("expo/metro-config");
 module.exports = (async () => {
   let defaultConfig = await getDefaultConfig(__dirname);
   defaultConfig.resolver.resolverMainFields.unshift("sbmodern");
+  defaultConfig.resolver.sourceExts.push("mjs");
   return defaultConfig;
 })();
+
+// import { getDefaultConfig } from "expo/metro-config";
+
+// export default async () => {
+//   const defaultConfig = await getDefaultConfig(__dirname);
+
+//   return {
+//     resolver: {
+//       // assetExts: [...defaultConfig.resolver.assetExts, 'db'],
+//       sourceExts: [...defaultConfig.resolver.sourceExts, "mjs"],
+//       resolverMainFields: [
+//         ...defaultConfig.resolver.resolverMainFields,
+//         "sbmodern",
+//       ],
+//     },
+//   };
+// };
--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
 export type MyButtonProps = {
   onPress: () => void;
@@ -13,22 +14,32 @@ export const MyButton: React.FC<MyButtonProps> = ({
   disabled = false,
 }) => {
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      disabled={disabled}
-      style={[styles.container, {opacity: disabled ? 0.3 : 1}]}
-    >
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
+    <Container onPress={onPress} disabled={disabled}>
+      <Text>{text}</Text>
+    </Container>
+    // <TouchableOpacity
+    //   onPress={onPress}
+    //   disabled={disabled}
+    //   style={[styles.container, {opacity: disabled ? 0.3 : 1}]}
+    // >
+    //   <Text style={styles.text}>{text}</Text>
+    // </TouchableOpacity>
   );
 };
 
- const styles = StyleSheet.create({
-   container: {
-     paddingHorizontal: 16,
-     paddingVertical: 8,
-     backgroundColor: "purple",
-     borderRadius: 8,
-   },
-   text: { color: "white" },
- });
+const Container = styled.TouchableOpacity`
+  padding: 8px 16px;
+  background-color: purple;
+  border-radius: 8px;
+  color: white;
+`;
+
+//  const styles = StyleSheet.create({
+//    container: {
+//      paddingHorizontal: 16,
+//      paddingVertical: 8,
+//      backgroundColor: "purple",
+//      borderRadius: 8,
+//    },
+//    text: { color: "white" },
+//  });
